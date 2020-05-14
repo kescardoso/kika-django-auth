710783bfd790a135ecdc5dba6475e15c7de52cd4
Kes Cardoso <kesia.development@gmail.com>
2020-05-14 18:01:30 +0000

    Troubleshoot gmail smtp

--- kika_django_auth/settings.py
+++ kika_django_auth/settings.py
@@ -114,11 +114,13 @@ MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 # gmail smtp
-EMAIL_USE_TLS = True
+
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get('EMAIL_ADDRESS')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
-EMAIL_PORT = 587
+
+EMAIL_USE_SSL = True
+EMAIL_PORT = 465
 
 # sendgrid smtp
 # EMAIL_HOST = 'smtp.sendgrid.net'
