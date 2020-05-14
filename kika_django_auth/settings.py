94ebc69c88291b50abdff5c838cd551a2ba4318d
Kes Cardoso <kesia.development@gmail.com>
2020-05-14 18:40:02 +0000

    Test gmail smtp

--- kika_django_auth/settings.py
+++ kika_django_auth/settings.py
@@ -112,20 +112,11 @@ STATIC_URL = '/static/'
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-
-# gmail smtp
-# EMAIL_USE_SSL = True
-# EMAIL_PORT = 465
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_HOST_USER = os.environ.get('EMAIL_ADDRESS')
-# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
-
-# sendgrid smtp
-EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = 'apikey'
-EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_API_KEY')
-EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_ADDRESS')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
