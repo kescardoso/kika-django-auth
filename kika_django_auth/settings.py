581dfd4e7cafe7a7649151110aae04f72e8cb43e
Kes Cardoso <kesia.development@gmail.com>
2020-05-14 13:26:12 +0000

    Set environment variables for email handling

--- kika_django_auth/settings.py
+++ kika_django_auth/settings.py
@@ -109,14 +109,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-# Test reset password email from the backend:
-# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-
-# Send real email:
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+EMAIL_HOST_USER = os.environ.get('EMAIL_ADDRESS')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 EMAIL_PORT = 587
