86db8a83167104c373a5b05eb0fd5e1d9a5b4e9a
Kes Cardoso <kesia.development@gmail.com>
2020-05-14 15:48:25 +0000

    Add email authentication functionality from the backend

--- accounts/views.py
+++ accounts/views.py
@@ -5,17 +5,15 @@ from django.contrib.auth.models import User
 from accounts.forms import UserLoginForm, UserRegistrationForm
 
 
-# Create your views here.
 def index(request):
     """Return the index.html file"""
-    return render(request, 'index.html')
-
+    return render(request,  'index.html')
 
 @login_required
 def logout(request):
     """Log the user out"""
     auth.logout(request)
-    messages.success(request, "You have successfully been logged out!")
+    messages.success(request, "You have successfully been logged out")
     return redirect(reverse('index'))
 
 
@@ -25,18 +23,20 @@ def login(request):
         return redirect(reverse('index'))
     if request.method == "POST":
         login_form = UserLoginForm(request.POST)
+
         if login_form.is_valid():
             user = auth.authenticate(username=request.POST['username'],
-                                     password=request.POST['password'])
+                                    password=request.POST['password'])
+            messages.success(request, "You have successfully logged in!")
+
             if user:
                 auth.login(user=user, request=request)
-                messages.success(request, "You have successfully logged in!")
                 return redirect(reverse('index'))
             else:
-                login_form.add_error(None, "Username or password incorrect.")
+                login_form.add_error(None, "Your username or password is incorrect")
     else:
-        login_form = UserLoginForm
-    return render(request, 'login.html', {"login_form": login_form})
+        login_form = UserLoginForm()
+    return render(request, 'login.html', {'login_form': login_form})
 
 
 def registration(request):
@@ -52,22 +52,19 @@ def registration(request):
 
             user = auth.authenticate(username=request.POST['username'],
                                      password=request.POST['password1'])
-
             if user:
                 auth.login(user=user, request=request)
                 messages.success(request, "You have successfully registered")
-                return redirect(reverse('index'))
             else:
-                messages.error(request,
-                               "Unable to register your account at this time")
+                messages.error(request, "Unable to register your account at this time")
     else:
         registration_form = UserRegistrationForm()
-
-    return render(request, 'registration.html',
-                  {"registration_form": registration_form})
+    return render(request, 'registration.html', {
+        "registration_form": registration_form})
 
 
 def user_profile(request):
     """The user's profile page"""
     user = User.objects.get(email=request.user.email)
     return render(request, 'profile.html', {"profile": user})
+    
